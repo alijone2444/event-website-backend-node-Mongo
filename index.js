64f454baeae9bdba461204547b00e1d4fe53e22c
ali <alijone2444@gmail.com>
2023-07-20 04:03:27 +0500

    applied auth to login

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
-
+const LocalStrategy = require('passport-local').Strategy; // Add this line to import LocalStrategy
 //framework of node js to simply the making of api
 const app = express();
+const passport = require("passport");
+const session = require('express-session');
 
-//using cors for cross origin communication
-app.use(cors({
-  origin: 'http://localhost:3000',
-  credentials: true
-}));
-app.use(express.json());
+
+
+
 
 // Connect to MongoDB using Mongoose middleware / package
 mongoose.connect('mongodb+srv://alijone2333:rqgxCTarBj3TX6Bi@cluster1.rbjs7ls.mongodb.net/EventWebDb', {
@@ -23,7 +22,6 @@ mongoose.connect('mongodb+srv://alijone2333:rqgxCTarBj3TX6Bi@cluster1.rbjs7ls.mo
   .catch((error) => {
     console.error('Failed to connect to MongoDB', error);
   });
-
 // Create a schema for login
 const loginSchema = new mongoose.Schema({
     username: {
@@ -38,26 +36,65 @@ const loginSchema = new mongoose.Schema({
   });
   const LoginAuthenticate = mongoose.model('AuthenticateUsers', loginSchema, 'AuthenticateUsers');
   
-//requeest send from the login form handling
-app.get('/login', (req, res) => {
-    console.log(req.query);  
-  LoginAuthenticate.findOne({ username: req.query.username })
-    .then(user => {
-    if (user) {
-        console.log("User found");
-        // Perform actions for successful authentication
-        res.status(200).json({ message: "Authentication successful", success: true });
-      } else {
-        console.log("User not found");
-        // Perform actions for unsuccessful authentication
-        res.status(404).json({ message: "Authentication failed", success: false });
-      }
-    })
-    .catch(error => {
-    console.error(error);
-    res.status(500).json({ message: "Internal Server Error" });
-  });
+
+
+
+
+//using cors for cross origin communication
+app.use(cors({
+  origin: 'http://localhost:3000',
+  credentials: true
+}));
+app.use(express.json());
+app.use(passport.initialize());
+app.use(session({
+  secret: 'my-event-secret-key', // Replace with your actual secret key
+  resave: false,
+  saveUninitialized: false,
+}));
+app.use(passport.session());
+passport.serializeUser(function(user, done) {
+  done(null, user);
 });
+
+passport.deserializeUser(function(user, done) {
+  done(null, user);
+});
+//auth function
+passport.use(
+  "local-login",
+  new LocalStrategy(
+    {
+      usernameField: "username",
+      passwordField: "password",
+    },
+    async (username, password, done) => {
+      try {
+        const user = await LoginAuthenticate.findOne({ username: username });
+        if (!user) return done(null, false);
+        const isMatch = await LoginAuthenticate.findOne({ username: username ,password:password});
+        if (!isMatch)
+          return done(null, false);
+        // if passwords match return user
+        return done(null, user);
+      } catch (error) {
+        console.log(error)
+        return done(error, false);
+      }
+    }
+  )
+);
+
+
+
+//requeest send from the login form handling
+app.get('/login', passport.authenticate('local-login'),(req, res) => {
+    console.log(req.query);  console.log(req.session);
+    res.send({success:true})
+});
+
+
+
 
 
 //this is the port on which the server site runs
